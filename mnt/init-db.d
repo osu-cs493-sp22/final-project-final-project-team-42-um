aabd50bba054f17c12dcb64ec193769fd3059e84
binarybrain11 <zatcat6@gmail.com>
2022-06-04 19:04:21 -0700

    Refactored and working init script

--- mnt/init-db.d
+++
@@ -1,7 +0,0 @@
-// Add a Tarpaulin user to the Tarpaulin database
-db = db.getSiblingDB("Tarpaulin")
-db.createUser({
-    user: "Tarpaulin",
-    pwd: "hunter2",
-    roles: [ { role: "readWrite", db: "Tarpaulin" }]
-});
