1dc781bc939ec84e75bf94acea9750b760d3d82e
binarybrain11 <zatcat6@gmail.com>
2022-06-05 01:25:02 -0700

    assignment post, get, and patch are done

--- api/assignments.js
+++ api/assignments.js
@@ -20,10 +20,11 @@ const router = Router()
 router.post('/', requireAuthentication, async (req, res, next) => {
     const assignment = req.body 
     if (assignment.courseId){
+        // Check authentication status
         const course = await models.course.findById(assignment.courseId)
         if (course) {
-            console.log(course)
             if (course.instructorId == req.user || req.role === roles.admin) {
+                // Finally create assignment
                 models.assignment.create(assignment).then( newAss => {
                     res.status(201).json({
                         id: newAss._id
@@ -51,12 +52,52 @@ router.post('/', requireAuthentication, async (req, res, next) => {
 
 // Fetch data about a specific assignment
 router.get('/:id', (req, res, next) => {
-    next()
+    models.assignment.findById(req.params.id).then( (assignment) => {
+        if (assignment) {
+            res.status(200).send(assignment)
+        } else {
+            next()
+        }
+    }).catch( err => res.status(400).send( errorHandler(err) ) )
 })
 
 // Update data for a specific assignment
-router.patch('/:id', (req, res, next) => {
-    next()
+router.patch('/:id', requireAuthentication, (req, res, next) => {
+    models.assignment.findById(req.params.id).then( assignment => {
+        if (assignment) {
+            // Update assignment fields
+            Object.keys(req.body).every(
+                key => assignment[key] = req.body[key]
+            )
+            // Check authenication status
+            models.course.findById(assignment.courseId).then( course => {
+                if (course) {
+                    if (req.role === roles.admin || req.user == course.instructorId){
+                        // Finally update assignment
+                        assignment.save().then( () => {
+                            res.status(200).send()
+                        }).catch( 
+                            err => res.status(400).send( errorHandler(err) ) 
+                        )
+                    } else {
+                        res.status(403).json({
+                            error: "Only an authorized user can modify an assignment"
+                        })
+                    }
+                } else {
+                    res.status(400).json({
+                        error: "Assignment courseId doesn't exist"
+                    })
+                }
+            }).catch( 
+                err => res.status(400).send( errorHandler(err) )
+            )
+        } else {
+            next()
+        }
+    }).catch( 
+        err => res.status(400).send(errorHandler(err))
+    )
 })
 
 // Remove a specific assignment from the database
